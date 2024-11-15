376cf3478cab0b06cb06711554fbad4deef70b11
Stibysh_T <13zayavka@gmail.com>
2024-11-15 14:54:09 +0200

    hw-9-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 
+let formData = { email: '', message: '' };
+const LS_KEY = 'feedback-form-state';
+
+formData = JSON.parse(localStorage.getItem(LS_KEY));
+input.value = formData.email ?? '';
+textarea.value = formData.message ?? '';
+
+form.addEventListener('input', handlerInput);
+function handlerInput(event) {
+  formData.email = input.value.trim();
+  formData.message = textarea.value.trim();
+  localStorage.setItem(LS_KEY, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault();
+  if (!input.value || !textarea.value) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(LS_KEY);
+    form.reset();
+  }
+}
